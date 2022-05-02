951d89ffd66367dddbda4204ec9d0781dd7be595
AlanAloha <zby991023@gmail.com>
2022-05-02 12:09:16 -0700

    ini

--- makesnake.py
+++ makesnake.py
@@ -1,1 +1,12 @@
 # Make shakefiles
+import argparse
+
+parser = argparse.ArgumentParser(description='Make Snakefiles')
+parser.add_argument('--aligner', type=str, metavar='<str>', required=False,
+	help='input sequence aligner {bwa_mem, bowtie2, STAR} (default: bwa_mem)')
+parser.add_argument('-nd', '--no_deduplication', action='store_true',
+	help='if tagged, bam files will not be deduplicated')
+# call peak
+# report
+# ...
+arg = parser.parse_args()
